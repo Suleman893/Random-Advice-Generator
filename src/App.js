d5007c4f452c23ef0f87d1fc40e705de1001c48d
shahid-cloud-hub <sulemanahmad.webevis@gmail.com>
2022-04-17 17:54:58 +0500

    Fetch api and displayed on card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import React from "react";
+import axios from "axios";
 import "./App.css";
 
 class App extends React.Component {
+  state = {
+    advice: "",
+  };
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((res) => {
+        const {advice} = res.data.slip;
+
+        this.setState({advice: advice});
+      })
+      .catch((err) => {
+        console.error(`The error while fetching api ${err}`);
+      });
+  };
   render() {
-    return <h1>App</h1>;
+    const {advice} = this.state;
+    return (
+      <div className="app">
+        <div className="card">
+          <h1 className="heading"> {advice}</h1>
+          <button className="button" onClick={this.fetchAdvice}>
+            <span>Give me Advice </span>
+          </button>
+        </div>
+      </div>
+    );
   }
 }
 
